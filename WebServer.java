ad2062d1f0b6aac3109969b2c44efc2130f548ed
khiney <79067766+khiney@users.noreply.github.com>
2022-02-14 16:24:35 -0700

    Add files via upload

--- WebServer.java
+++ WebServer.java
@@ -195,28 +195,20 @@ class WebServer {
           Map<String, String> query_pairs = new LinkedHashMap<String, String>();
           // extract path parameters
           query_pairs = splitQuery(request.replace("multiply?", ""));
-          int num1;
-          int num2;
+          Integer num1;
+          Integer num2;
           try{
-            // extract required fields from parameters
-            num1 = Integer.parseInt(query_pairs.get("num1"));
-            num2 = Integer.parseInt(query_pairs.get("num2"));
-
-            // do math
-            int result = num1 * num2;
+            num1= Integer.parseInt(query_pairs.get("num1"));
+            num2= Integer.parseInt(query_pairs.get("num2"));
+            Integer result = num1 * num2;
 
             // Generate response
             builder.append("HTTP/1.1 200 OK\n");
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
             builder.append("Result is: " + result);
-          }catch(NumberFormatException ie){
-            num1=1;
-            num2=0;
-            builder.append("HTTP/1.1 400 Bad Request\n");
-            builder.append("Content-Type: text/html; charset=utf-8\n");
-            builder.append("\n");
-            builder.append("Please enter an integer");
+          }catch (NumberFormatException ne){
+            ne.printStackTrace();
           }
 
           // TODO: Include error handling here with a correct error code and
