b9a11b457fc236759a11accf163c06b618ec85c8
khiney <79067766+khiney@users.noreply.github.com>
2022-02-14 15:27:21 -0700

    Add files via upload

--- WebServer.java
+++ WebServer.java
@@ -210,7 +210,7 @@ class WebServer {
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
             builder.append("Result is: " + result);
-          }catch (NumberFormatException ne){
+          }catch (NumberFormatException ignored){
           }
 
           // do math
