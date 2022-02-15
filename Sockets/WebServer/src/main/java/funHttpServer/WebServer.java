b10bee511eabd4264681ebc19d25b61079bbb5bf
khiney <79067766+khiney@users.noreply.github.com>
2022-02-14 19:46:22 -0700

    Add files via upload

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -251,7 +251,7 @@ class WebServer extends Object{
             builder.append("Check the todos mentioned in the Java source file");
             // TODO: Parse the JSON returned by your fetch and create an appropriate
             JSONObject newObject = new JSONObject(json);
-            System.out.println(newObject.getSJONObject("owner").getString("owner"));
+            System.out.println(newObject.getJSONObject("owner").getString("owner"));
             // response
             // and list the owner name, owner id and name of the public repo on your webpage, e.g.
             // amehlhase, 46384989 -> memoranda
