3c7a948b9479cb0800326b244717162b4d24d1e2
khiney <79067766+khiney@users.noreply.github.com>
2022-02-14 22:41:03 -0700

    Add files via upload

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -310,7 +310,7 @@ class WebServer extends Object{
             builder.append("HTTP/1.1 400 Bad Request\n");
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
-            builder.append("Please enter a valid integer");
+            builder.append("Please enter a word");
           }
 
 
